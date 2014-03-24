f50d31267f872d9f9ac49f542e606eb5d293749a
john.chen <john.chen@fmkj.net>
2014-03-24 10:59:24 +0800

    update

---
+++ project/icom/qti/qtiA.js
@@ -0,0 +1,15 @@
+/**
+ * Created with Vim7.3 Ubuntu10.04
+ * @fileOverview : 
+ * @author : Chen weihan <csq-3@163.com>
+ * @since : 2014年03月24日 星期一 10时21分12秒
+ * @filename : project/icom/qti/qtiA.js
+ * @version : 
+ * @description : 
+ */
+
+var qitA = {};
+ 
+    exports.getQti =  function(req,res) {
+        console.log('api success!');
+    }
