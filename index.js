72c3247f39521bfb38337f2d982a93868aef6eed
john.chen <john.chen@fmkj.net>
2014-03-27 16:30:35 +0800

    rest

--- index.js
+++ index.js
@@ -16,7 +16,16 @@
  * 支持websock
  * 支持验证
  * 错误输出到界面上
+
+ * //常规使用
+ * eg : http://127.0.0.1:1337/index.js?r=json/icom.qti/qtiA/getQti&info={"a":1,"b":2,"c":3,"d":4,"e":5} 
+ *      r = 返回类型/目录层级/页面名称(页面名称与类名一致)/函数名
+ * 
+ * REST调用,在rest函数里，调用rest函数API,原理于上面相同，下面函数命名,使用方式更规范,qtiA url代表资源为qtiA的url唯一标识.rest代表是REST方式的API。
+ * eg : http://127.0.0.1:1337/index.js?r=json/icom.qti/qtiA/rest&info={"a":1,"b":2,"c":3,"d":4,"e":5} 
+ * 也就是说如果上面请求是get 那么他会去请求qtiA模块里的restRead函数,post 对应restCreate函数，具体看demo
  */
+
 
 
 /**
--- project/icom/qti/qtiA.js
+++ project/icom/qti/qtiA.js
@@ -1,7 +1,7 @@
 /*;
  * Created with Vim7.3 Ubuntu10.04
- * @fileOverview : 
- * @author : Chen weihan <csq-3@163.com>
+ * @fileOverview : demo案例 
+ * @author : Chen weihan <csq-3@163.com> 
  * @since : 2014年03月24日 星期一 10时21分12秒
  * @filename : project/icom/qti/qtiA.js
  * @version : 
@@ -12,7 +12,6 @@ var paramData = require(global.ENV.libDir+'param.js'),
     db = require(global.ENV.libDir+'db.js'),
     debug = require(global.ENV.libDir + 'debug.js');
 
-
     //http://127.0.0.1:1337/index.js?r=json/icom.qti/qtiA/getQti&info={"a":1,"b":2,"c":3,"d":4,"e":5} 
     exports.getQti =  function(req,res) {
           paramData.param(req,res,function(res,param){
@@ -22,27 +21,72 @@ var paramData = require(global.ENV.libDir+'param.js'),
           });
     };
 
-    //http://127.0.0.1:1337/index.js?r=json/icom.qti/qtiA/selectSql&info={id:1}
-    exports.selectSql = function(req,res) {
-       
+    //http://127.0.0.1:1337/index.js?r=json/icom.qti/qtiA/getSql&info={id:1}
+    exports.getSql = function(req,res) {
           //获取参数  
           paramData.param(req,res,function(res,param){
                var id = param['id'],
                    sql = 'SELECT * FORM test WHERE id =' + id;
-               
                //数据库查询 
                db.query(sql,function(rs){
-                     console.log('selectSql',rs); 
+                     console.log('getSql',rs); 
                      debug.print(JSON.stringify(rs),res);
                },res);
-
           });
-
-
-
-         var sql = ' SELECT * form test ';
- 
-
-
-
     };
+
+    //http://127.0.0.1:1337/index.js?r=json/icom.qti/qtiA/postSql
+    //post:info={id:1}
+    exports.postSql = function(req,res) {
+          //获取参数  
+          paramData.param(req,res,function(res,param){
+               var id = param['id'],
+                   sql = 'SELECT * FORM test WHERE id =' + id;
+               //数据库查询 
+               db.query(sql,function(rs){
+                     console.log('postSql',rs); 
+                     debug.print(JSON.stringify(rs),res);
+               },res);
+          });
+    };
+
+    //回调多嵌套处理
+    exports.callbacks = function(req,res) {
+    
+    };
+
+    /**
+     * REST标准调用
+     * eg : http://127.0.0.1:1337/index.js?r=json/icom.qti/qtiA/rest&info={"a":1,"b":2,"c":3,"d":4,"e":5} 
+     * 如果最后函数是REST系统会针对提交方式直接调用对于的方法
+     *      
+     */
+
+    // POST -> Create
+    exports.restCreate = function(req,res) {
+   
+    };
+    
+    // PUT -> Update
+    exports.restUpdate = function(req,res) {
+    
+    };
+
+    // DELETE -> Delete
+    exports.restDelete = function(req,res) {
+    
+    };
+
+    // GET -> Read
+    exports.restRead = function(req,res) {
+          paramData.param(req,res,function(res,param){
+              console.log('restCreate data',param);
+              res.writeHead(200, {'Content-Type': 'text/html;charset=UTF-8'});
+              res.end(JSON.stringify(param));
+          });
+    };
+
+
+
+
+
