f9a43ad260d6ae7ab1843363841e468f94fbb3b5
john.chen <john.chen@fmkj.net>
2014-03-27 11:34:58 +0800

    mysql nodejs

--- project/icom/qti/qtiA.js
+++ project/icom/qti/qtiA.js
@@ -9,13 +9,40 @@
  */
 
 var paramData = require(global.ENV.libDir+'param.js'),
-    db = require(global.ENV.libDir+'db.js');
- 
+    db = require(global.ENV.libDir+'db.js'),
+    debug = require(global.ENV.libDir + 'debug.js');
+
+
+    //http://127.0.0.1:1337/index.js?r=json/icom.qti/qtiA/getQti&info={"a":1,"b":2,"c":3,"d":4,"e":5} 
     exports.getQti =  function(req,res) {
-          db.mysqlDB(res);
           paramData.param(req,res,function(res,param){
-              console.log('api data',param);
+              console.log('getQti data',param);
               res.writeHead(200, {'Content-Type': 'text/html;charset=UTF-8'});
               res.end(JSON.stringify(param));
           });
+    };
+
+    //http://127.0.0.1:1337/index.js?r=json/icom.qti/qtiA/selectSql&info={id:1}
+    exports.selectSql = function(req,res) {
+       
+          //获取参数  
+          paramData.param(req,res,function(res,param){
+               var id = param['id'],
+                   sql = 'SELECT * FORM test WHERE id =' + id;
+               
+               //数据库查询 
+               db.query(sql,function(rs){
+                     console.log('selectSql',rs); 
+                     debug.print(JSON.stringify(rs),res);
+               },res);
+
+          });
+
+
+
+         var sql = ' SELECT * form test ';
+ 
+
+
+
     };
